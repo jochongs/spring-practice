40a05ae0bcbdc6133c14d03cee23a91e4d3974c0
jochongs <joch2712@naver.com>
2024-03-22 20:31:16 +0900

    chore: add code comment about bean annotation

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -9,7 +9,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
     // Java 코드로 직접 설정
-    // 장점
+    // 실무에서는 주로 정형화된 컴포넌트 스캔 방식 사용
+    // 정형화 되지 않거나, 상황에 따라 구현 클래스를 변경해야 하면 설정을 통해 스프링 빈 사용
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
