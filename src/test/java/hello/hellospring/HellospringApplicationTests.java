f7718f86b4d3d939fc4b419a601fc3fc4553405e
jochongs <joch2712@naver.com>
2024-03-22 18:07:09 +0900

    add test case

--- src/test/java/hello/hellospring/HellospringApplicationTests.java
+++ src/test/java/hello/hellospring/HellospringApplicationTests.java
@@ -9,5 +9,4 @@ class HellospringApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -4,6 +4,8 @@ import hello.hellospring.domain.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 class MemberServiceTest {
 
     MemberService memberService = new MemberService();
@@ -20,7 +22,30 @@ class MemberServiceTest {
         Long saveId = memberService.join(member);
 
         // then
-        Assertions.assertThat(saveId).isEqualTo(member.getId());
+        Member findMember = memberService.findOne(saveId).get();
+        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void join_member_duplicate() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        memberService
+        //when
+        /*memberService.join(member1);
+        try {
+            memberService.join(member2);
+            Assertions.fail("fail");
+        } catch(IllegalStateException exception) {
+            Assertions.assertThat(exception.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        }*/
+
+        //then
     }
 
     @Test
