7c601987a1c50dd83f297ba67a6c20eb2ccb514d
jochongs <joch2712@naver.com>
2024-03-22 18:48:55 +0900

    feat: modify DI with auto wired

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,5 +1,24 @@
 package hello.hellospring.controller;
 
-public class MemberController {
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(
+        MemberService memberService
+    ) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping("")
+    public void signUp() {
+
+    }
 }
